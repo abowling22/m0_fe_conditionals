7bfb81292cf027a6e4d72832b58c1e6d2c935caf
Drew Bowling <andrewwbowling@gmail.com>
2023-03-02 14:59:55 -0700

    make corrections on challenge 2

---
+++ codeChallenges/challenge3.js
@@ -0,0 +1,13 @@
+var number = 15
+
+if (number % 3 === 0 && number % 5 === 0) {
+ console.log("fizzbuzz")   
+} else if (number % 3 === 0) {
+    console.log ("Fizz")
+} else if (number % 5 === 0){
+    console.log("Buzz")
+} else {
+    console.log(number)
+}
+
+//completed morning of 3/2
